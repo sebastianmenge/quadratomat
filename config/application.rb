7a8979a70d2a1e7e3a3258c0a2d2f4b969e02d59
sebastianmenge <an.sebastian.menge@gmail.com>
2015-02-14 20:02:56 +0100

    add sharing tags

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module Quadratomat
   class Application < Rails::Application
 
     config.react.addons = true
+    config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
